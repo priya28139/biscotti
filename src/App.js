458c3c9842e90cb173866b1d33ab362013b02aa0
Priya Aswani <priya.aswani28139@gmail.com>
2021-03-28 01:07:36 +0400

    Fetch recipes from Edamam API and set state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import AppBar from "./components/AppBar"
 
 const App = () => {
   const APP_ID = "b55d2702"
   const APP_KEY = "24c1e01a946fc7309dacda08beb748bb"
   const APP_URI = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(APP_URI)
+    const data = await response.json()
+    console.log(data.hits)
+    setRecipes(data.hits)
+  }
 
   return (
     <>
