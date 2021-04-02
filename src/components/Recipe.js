b4f95d7e4bfc8e47464dc1ad6109600c035d63b6
Priya Aswani <priya.aswani28139@gmail.com>
2021-04-02 13:04:55 +0400

    Set up basic layout of Recipe.js

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,42 @@
 import React from "react";
-const Recipe = ({ match }) => {
-  const id = match.params.id;
-  return <h1>Recipe ID: {id}</h1>;
-};
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-export default Recipe;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Recipe({ match }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={3}
+        style={{
+          width: "calc(100% - 1em)",
+          margin: "1em auto",
+        }}
+      >
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
