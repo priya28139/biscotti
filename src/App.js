e4969203f4ff23c5987b8c2749eadb41e21a1a10
Priya Aswani <priya.aswani28139@gmail.com>
2021-04-02 12:51:49 +0400

    Add Router, Switch & Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AppBar from "./components/AppBar";
 import RecipeList from "./components/RecipeList";
+import Recipe from "./components/Recipe";
 
 const App = () => {
   const APP_URI = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
@@ -17,10 +19,15 @@ const App = () => {
   };
 
   return (
-    <>
+    <Router>
       <AppBar setSearchString={setSearchString} />
-      <RecipeList meals={recipes} />
-    </>
+      <Switch>
+        <Route path="/" exact>
+          <RecipeList meals={recipes} />
+        </Route>
+        <Route path="/recipes/:id" component={Recipe}></Route>
+      </Switch>
+    </Router>
   );
 };
 
