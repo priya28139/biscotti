9532f50ca8bc8f42988fd7a66a4ce36ced33e75d
Priya Aswani <priya.aswani28139@gmail.com>
2021-04-04 01:32:06 +0400

    Import RecipeContext and wrap App within Provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AppBar from "./components/AppBar";
 import RecipeList from "./components/RecipeList";
 import Recipe from "./components/Recipe";
+import { RecipeContext, RecipeProvider } from "./context/RecipeContext";
 
-const App = () => {
+function App() {
   const APP_URI = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
   const [recipes, setRecipes] = useState([]);
   const [searchString, setSearchString] = useState("");
+  const recipeContext = useContext(RecipeContext);
+  const { favorites, setFavorites } = recipeContext;
+
   useEffect(() => {
     getRecipes();
   }, [searchString]);
@@ -26,9 +30,18 @@ const App = () => {
           <RecipeList meals={recipes} />
         </Route>
         <Route path="/recipes/:id" component={Recipe}></Route>
+        <Route path="/favorites">
+          <RecipeList meals={{ meals: favorites }} />
+        </Route>
       </Switch>
     </Router>
   );
-};
+}
 
-export default App;
+export default () => {
+  return (
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>
+  );
+};
