e0c07645422329ba154b0c18ec89cbc7c9e58de4
Priya Aswani <priya.aswani28139@gmail.com>
2021-04-04 14:03:32 +0400

    Add recipes and searchString to RecipeContext

--- src/context/RecipeContext.js
+++ src/context/RecipeContext.js
@@ -2,12 +2,19 @@ import React, { useState, useContext } from "react";
 export const RecipeContext = React.createContext();
 
 export const RecipeProvider = ({ children }) => {
+  const [recipes, setRecipes] = useState([]);
   const [favorites, setFavorites] = useState([]);
+  const [searchString, setSearchString] = useState("");
+
   return (
     <RecipeContext.Provider
       value={{
         favorites,
         setFavorites,
+        recipes,
+        setRecipes,
+        searchString,
+        setSearchString,
       }}
     >
       {children}
