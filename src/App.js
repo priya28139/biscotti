48eba11f899b84b9155d167c5c71ebb89fc96c7e
Priya Aswani <priya.aswani28139@gmail.com>
2021-04-04 14:02:38 +0400

    Fetch all recipes by querying with first letter API

--- src/App.js
+++ src/App.js
@@ -6,28 +6,89 @@ import Recipe from "./components/Recipe";
 import { RecipeContext, RecipeProvider } from "./context/RecipeContext";
 
 function App() {
-  const APP_URI = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
-  const [recipes, setRecipes] = useState([]);
-  const [searchString, setSearchString] = useState("");
+  const APP_URI = `https://www.themealdb.com/api/json/v1/1/search.php?f=`;
   const recipeContext = useContext(RecipeContext);
-  const { favorites, setFavorites } = recipeContext;
-
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
+  const {
+    favorites,
+    setFavorites,
+    recipes,
+    setRecipes,
+    searchString,
+    setSearchString,
+  } = recipeContext;
   useEffect(() => {
     getRecipes();
-  }, [searchString]);
+  }, []);
 
   const getRecipes = async () => {
-    const response = await fetch(APP_URI + searchString);
-    const data = await response.json();
-    setRecipes(data);
+    let allRecipes = [];
+    const allLetters = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    let firstLetter = "";
+    for (var i = 0; i < allLetters.length; i++) {
+      firstLetter = allLetters[i];
+      const response = await fetch(APP_URI + firstLetter);
+      const data = await response.json();
+      if (data) {
+        console.log(data.meals);
+        for (var j = 0; j < data.meals?.length; j++) {
+          allRecipes.push(data.meals[j]);
+          setRecipes({ meals: allRecipes });
+        }
+      }
+    }
   };
+
+  useEffect(() => {
+    setFilteredRecipes(recipes);
+  }, [recipes]);
+
+  useEffect(() => {
+    if (searchString === "") {
+      setFilteredRecipes(recipes);
+    } else {
+      const filtered = recipes.meals?.filter((meal) => {
+        return (
+          meal.strMeal.toString().toLowerCase().indexOf(searchString) !== -1
+        );
+      });
+      setFilteredRecipes({ meals: filtered });
+    }
+  }, [searchString]);
 
   return (
     <Router>
-      <AppBar setSearchString={setSearchString} />
+      <AppBar />
       <Switch>
         <Route path="/" exact>
-          <RecipeList meals={recipes} />
+          <RecipeList meals={filteredRecipes} />
         </Route>
         <Route path="/recipes/:id" component={Recipe}></Route>
         <Route path="/favorites">
