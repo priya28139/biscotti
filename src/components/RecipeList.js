e9d4b136cc2032cab2795f39b3f8c75c61b108c1
Priya Aswani <priya.aswani28139@gmail.com>
2021-04-23 13:30:11 +0400

    Implement searching of recipes/favorites in RecipeList

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import RecipeCard from "./RecipeCard";
 import { Container } from "@material-ui/core";
 import RecipeCardAlternative from "./RecipeCardAlternative";
+import { RecipeContext } from "../context/RecipeContext";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,15 +20,61 @@ const useStyles = makeStyles((theme) => ({
 
 export default function RecipeList(props) {
   const classes = useStyles();
-  const { meals } = props.meals;
-  let counter = -1;
-  const recipeCards = meals?.map((meal) => {
-    counter++;
-    if (counter === 16) {
-      counter = 0;
+  const recipeContext = useContext(RecipeContext);
+  const { searchString, recipes, favorites } = recipeContext;
+  const [meals, setMeals] = useState([]);
+  const [recipeCards, setRecipeCards] = useState([]);
+
+  useEffect(() => {
+    if (props.type === "all") {
+      setMeals(recipes);
+    } else {
+      setMeals(favorites);
     }
-    return <RecipeCard meal={meal} counter={counter} key={meal.idMeal} />;
-  });
+  }, [recipes, favorites]);
+
+  useEffect(() => {
+    console.log("meals changed in RecipeList");
+    console.log(meals);
+    let counter = -1;
+    const recipeCardsLocal = meals?.meals?.map((meal) => {
+      counter++;
+      if (counter === 16) {
+        counter = 0;
+      }
+      return <RecipeCard meal={meal} counter={counter} key={meal.idMeal} />;
+    });
+
+    setRecipeCards(recipeCardsLocal);
+  }, [meals]);
+
+  useEffect(() => {
+    if (searchString === "") {
+      if (props.type === "all") {
+        setMeals(recipes);
+      } else {
+        setMeals(favorites);
+      }
+    } else {
+      let filtered = [];
+      if (props.type === "all") {
+        filtered = recipes?.meals?.filter((meal) => {
+          return (
+            meal.strMeal.toString().toLowerCase().indexOf(searchString) !== -1
+          );
+        });
+      } else {
+        filtered = favorites?.meals?.filter((meal) => {
+          return (
+            meal.strMeal.toString().toLowerCase().indexOf(searchString) !== -1
+          );
+        });
+      }
+
+      setMeals({ meals: filtered });
+    }
+  }, [searchString]);
+
   return (
     <Grid
       container
