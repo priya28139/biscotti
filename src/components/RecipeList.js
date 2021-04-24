f602bf1ac4ebbce34251c55e9c36404795c76963
Priya Aswani <priya.aswani28139@gmail.com>
2021-04-24 19:22:04 +0400

    Refactor RecipeList to work with the new favorites

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,44 +1,44 @@
 import React, { useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import RecipeCard from "./RecipeCard";
-import { Container } from "@material-ui/core";
-import RecipeCardAlternative from "./RecipeCardAlternative";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
 
 export default function RecipeList({ recipes, favorites, setFavorites, type }) {
-  const classes = useStyles();
-  const { meals } = type === "all" ? recipes : favorites;
   useEffect(() => {
     console.log("Favorites from RecipeList: ", favorites);
   }, [favorites]);
   let counter = -1;
-  const recipeCards = meals?.map((meal) => {
-    counter++;
-    if (counter === 16) {
-      counter = 0;
-    }
-    return (
-      <RecipeCard
-        meal={meal}
-        counter={counter}
-        key={meal.idMeal}
-        favorites={favorites}
-        setFavorites={setFavorites}
-      />
-    );
-  });
+  const recipeCards =
+    type === "all"
+      ? recipes.meals?.map((meal) => {
+          counter++;
+          if (counter === 16) {
+            counter = 0;
+          }
+          return (
+            <RecipeCard
+              meal={meal}
+              counter={counter}
+              key={meal.idMeal}
+              favorites={favorites}
+              setFavorites={setFavorites}
+            />
+          );
+        })
+      : favorites.map((meal) => {
+          counter++;
+          if (counter === 16) {
+            counter = 0;
+          }
+          return (
+            <RecipeCard
+              meal={meal}
+              counter={counter}
+              key={meal.idMeal}
+              favorites={favorites}
+              setFavorites={setFavorites}
+            />
+          );
+        });
   return (
     <Grid
       container
